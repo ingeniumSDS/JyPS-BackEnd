e023c185c6bbece8ba23af4ecf162f7221e45bc4
Krebs-et <chava.r.lol@gmail.com>
2026-03-19 22:54:48 -0600

    Se cambia configuración de CORS al formato de anotación

--- src/main/java/com/ingenium/jyps/users/infrastructure/adapters/in/web/UsuarioController.java
+++ src/main/java/com/ingenium/jyps/users/infrastructure/adapters/in/web/UsuarioController.java
@@ -15,6 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
+@CrossOrigin("*")
 public class UsuarioController {
 
     private final RegistrarUsuarioUseCase registrarUsuarioUseCase;
