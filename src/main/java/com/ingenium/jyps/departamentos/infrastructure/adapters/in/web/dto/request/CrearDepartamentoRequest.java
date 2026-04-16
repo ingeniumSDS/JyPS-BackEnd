f4f98767e67c9652b65178c72433db338719fff6
Krebs-et <chava.r.lol@gmail.com>
2026-04-16 10:32:25 -0600

    feat: Remove validation constraints for descripcion in CrearDepartamentoRequest

--- src/main/java/com/ingenium/jyps/departamentos/infrastructure/adapters/in/web/dto/request/CrearDepartamentoRequest.java
+++ src/main/java/com/ingenium/jyps/departamentos/infrastructure/adapters/in/web/dto/request/CrearDepartamentoRequest.java
@@ -3,16 +3,12 @@ package com.ingenium.jyps.departamentos.infrastructure.adapters.in.web.dto.reque
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Length;
 
 public record CrearDepartamentoRequest(
         @NotNull(message = "El nombre del departamento no puede ser nulo")
         @NotBlank(message = "El nombre del departamento no puede estar vacío")
         String nombre,
 
-        @Length(max = 255, min = 25, message = "La descripción del departamento no puede exceder los 255 caracteres ni ser menor a 25 caracteres")
-        @NotBlank(message = "La descripción del departamento no puede estar vacía")
-        @NotNull(message = "La descripción del departamento no puede ser nula")
         String descripcion,
 
         Long jefeId
