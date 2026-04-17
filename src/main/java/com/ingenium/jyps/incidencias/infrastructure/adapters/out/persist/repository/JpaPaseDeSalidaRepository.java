87469231f734e82a6f17e5ab0d184b26e9efdef8
Krebs-et <chava.r.lol@gmail.com>
2026-04-17 10:07:04 -0600

    feat: Add ordering to Justificante and PaseDeSalida repository queries

--- src/main/java/com/ingenium/jyps/incidencias/infrastructure/adapters/out/persist/repository/JpaPaseDeSalidaRepository.java
+++ src/main/java/com/ingenium/jyps/incidencias/infrastructure/adapters/out/persist/repository/JpaPaseDeSalidaRepository.java
@@ -1,6 +1,7 @@
 package com.ingenium.jyps.incidencias.infrastructure.adapters.out.persist.repository;
 
 import com.ingenium.jyps.incidencias.infrastructure.adapters.out.persist.entity.PaseDeSalidaEntity;
+import jakarta.persistence.OrderBy;
 import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,13 +12,15 @@ import java.util.Optional;
 
 public interface JpaPaseDeSalidaRepository extends JpaRepository<PaseDeSalidaEntity, Long> {
 
-
+    @OrderBy("fechaSolicitud DESC")
     List<PaseDeSalidaEntity> findByEmpleado_Id(Long empleadoId);
 
+    @OrderBy("fechaSolicitud ASC")
     List<PaseDeSalidaEntity> findByJefe_Id(Long jefeId);
 
     Optional<PaseDeSalidaEntity> findByQR(String qr);
 
+    @OrderBy("fechaSolicitud DESC")
     @Query("SELECT P FROM PaseDeSalidaEntity P WHERE (P.fechaSolicitud >= :fechaInicio " +
             "AND P.fechaSolicitud <= :fechaFin) AND P.estado IN ('A_TIEMPO', 'RETARDO')")
     List<PaseDeSalidaEntity> findByFechaSolicitudBetween(LocalDate fechaInicio, LocalDate fechaFin);
